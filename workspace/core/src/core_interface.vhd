71b23ed47d5bb3d060cec83ea3521c732c05f7ca
nilewapp <robert.welin@gmail.com>
2013-11-26 00:27:36 +0000

    Rename CoreInterface to core_interface

--- workspace/core/src/core_interface.vhd
+++ workspace/core/src/core_interface.vhd
@@ -2,16 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-package CoreInterface is
+package core_interface is
 
     constant WORD_LENGTH: integer := 18;
     constant REGISTER_ADDR_LENGTH: integer := 6;
+    constant RAM_ADDR_LENGTH: integer := 11;
     constant OPCODE_LENGTH: integer := 4;
 
     subtype word is std_logic_vector(WORD_LENGTH - 1 downto 0);
+    subtype register_addr is std_logic_vector(REGISTER_ADDR_LENGTH - 1 downto 0);
+    subtype ram_addr is std_logic_vector(RAM_ADDR_LENGTH - 1 downto 0);
     subtype opcode is std_logic_vector(OPCODE_LENGTH - 1 downto 0);
     
-end CoreInterface;
+end core_interface;
 
-package body CoreInterface is
+package body core_interface is
 end package body;
