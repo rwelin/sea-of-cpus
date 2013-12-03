96a8d39f950250d5495b41f849a4ea39acaae26d
nilewapp <robert.welin@gmail.com>
2013-12-03 00:44:22 +0000

    Add potential instruction set

--- workspace/core/src/core_interface.vhd
+++ workspace/core/src/core_interface.vhd
@@ -23,8 +23,23 @@ package core_interface is
         addrb: register_addr;
     end record;
 
-    constant OP_ADD: opcode := "0000";
-    
+    constant OP_NOP: opcode   := "0000"; --
+    constant OP_MOVE: opcode  := "0001"; -- | r1:6 | const:8 |
+    constant OP_LOAD: opcode  := "0010"; -- | r1:6 | addr:8 |
+    constant OP_STORE: opcode := "0011"; -- | r1:6 | addr:8 |
+    constant OP_J: opcode     := "0100"; -- | addr:11 |
+    constant OP_JR: opcode    := "0101"; -- | r1:6 | addr:8 |
+    constant OP_BEQ: opcode   := "0110"; -- | r1:6 | offset:8 |
+    constant OP_BNQ: opcode   := "0111"; -- | r1:6 | offset:8 |
+    constant OP_ADD: opcode   := "1000"; -- | r1:6 |
+    constant OP_SUB: opcode   := "1001"; -- | r1:6 |
+    constant OP_U1: opcode    := "1010";
+    constant OP_U2: opcode    := "1011";
+    constant OP_MAC: opcode   := "1100"; -- | r1:6 | r2:6 |
+    constant OP_MSC: opcode   := "1101"; -- | r1:6 | r2:6 |
+    constant OP_MULT: opcode  := "1110"; -- | r1:6 |
+    constant OP_U3: opcode    := "1111";
+
 end core_interface;
 
 package body core_interface is
