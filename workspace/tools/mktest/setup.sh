c749748b8fc9bc6c486c2d0638b2c9e28b50dbae
nilewapp <robert.welin@gmail.com>
2014-02-09 21:49:01 +0000

    Add makefiles and scripts for automating testing

---
+++ workspace/tools/mktest/setup.sh
@@ -0,0 +1,4 @@
+export XILINX=/opt/Xilinx/14.6/ISE_DS
+export PLATFORM=lin64
+export PATH=$PATH:${XILINX}/EDK/bin/${PLATFORM}:${XILINX}/ISE/bin/${PLATFORM}:${XILINX}/common/bin/${PLATFORM}:${XILINX}/PlanAhead/bin/${PLATFORM}
+export LD_LIBRARY_PATH=${XILINX}/ISE/lib/${PLATFORM}:${XILINX}/EDK/lib/${PLATFORM}:${XILINX}/common/lib/${PLATFORM}:${XILINX}/PlanAhead/lib/${PLATFORM}
