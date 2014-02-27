891dc645995dc780f45edc2dff3c139fa0377fe4
nilewapp <robert.welin@gmail.com>
2014-02-27 22:57:00 +0000

    Add instruction decode function stub and test

--- workspace/src/instruction_decode.vhd
+++ workspace/src/instruction_decode.vhd
@@ -3,11 +3,32 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.dsp_mode.all;
 use work.core_config.all;
+use work.opcodes.all;
 
 -- Functions relating to instruction decode.
 --
 package instruction_decode is
+
+    type DSPDataInputs is
+        record
+            accum: word;
+            reg_a: word;
+            reg_b: word;
+            mem_a: word;
+            const: word;
+        end record;
+
+    type DSPInputs is
+        record
+            mode: DSPMode;
+            a: std_logic_vector(29 downto 0);
+            b: std_logic_vector(17 downto 0);
+            c: std_logic_vector(47 downto 0);
+            d: std_logic_vector(24 downto 0);
+        end record;
+
     function sign_extend(v: std_logic_vector; len: integer) return std_logic_vector;
+    function decode_dsp_inputs(op: opcode; inputs: DSPDataInputs) return DSPInputs;
 end package instruction_decode;
 
 package body instruction_decode is
@@ -18,9 +39,29 @@ package body instruction_decode is
     begin
         assert len >= v'length
         report "sign_extend: output length must not be less than input length"
-        severity warning;
+        severity failure;
 
         return std_logic_vector(to_signed(to_integer(signed(v)), len));
     end function sign_extend;
+
+
+    -- Maps an opcode and a set of inputs to the ports of the DSP block.
+    --
+    function decode_dsp_inputs(op: opcode; inputs: DSPDataInputs) return DSPInputs is
+        variable output: DSPInputs;
+    begin
+        output.mode := DSP_C_PASSTHROUGH;
+        output.a := (others => '0');
+        output.b := (others => '0');
+        output.c := (others => '0');
+        output.d := (others => '0');
+        case op is
+            when OP_MOVA =>
+                output.c := inputs.const;
+            when others =>
+        end case;
+
+        return output;
+    end function decode_dsp_inputs;
 
 end package body instruction_decode;
--- workspace/test/instruction_decode_test.vhd
+++ workspace/test/instruction_decode_test.vhd
@@ -7,10 +7,14 @@ entity instruction_decode_test is
 end entity instruction_decode_test;
 
 architecture behav of instruction_decode_test is
+    signal sign_extend_test_done: std_logic := '0';
+    signal decode_dsp_inputs_test_done: std_logic := '0';
 begin
 
     sign_extend_test: process
     begin
+        wait for 1 ns;
+
         assert "0101" = sign_extend("0101", 4)
         report "Vector representing positive number extended to same length should result in the same vector."
         severity failure;
@@ -27,7 +31,24 @@ begin
         report "Vector representing negative number extended to longer length should result in the same vector with prepended ones."
         severity failure;
 
-        report "DONE" severity failure;
+        sign_extend_test_done <= '1';
     end process sign_extend_test;
+
+
+    decode_dsp_inputs_test: process
+    begin
+        wait for 1 ns;
+
+        decode_dsp_inputs_test_done <= '1';
+    end process decode_dsp_inputs_test;
+
+
+    done: process(sign_extend_test_done, decode_dsp_inputs_test_done)
+    begin
+        if sign_extend_test_done = '1' and
+           decode_dsp_inputs_test_done = '1' then
+            report "DONE" severity failure;
+        end if;
+    end process done;
 
 end architecture behav;
