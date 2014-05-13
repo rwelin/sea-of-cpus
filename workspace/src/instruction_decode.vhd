e4d5590e0d9ab589b575617ded1ed27eecb59bdb
Robert Welin <robert@weln.se>
2014-05-13 03:33:57 +0100

    Some cleanup to instruction_decode pkg

--- workspace/src/instruction_decode.vhd
+++ workspace/src/instruction_decode.vhd
@@ -3,21 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.dsp_mode.all;
 use work.core_config.all;
-use work.opcodes.all;
 use work.utils.all;
 
--- Functions relating to instruction decode.
---
 package instruction_decode is
-
-    type DSPDataInputs is
-        record
-            accum: word;
-            reg_a: word;
-            reg_b: word;
-            mem_a: word;
-            const: word;
-        end record;
 
     type DSPInputs is
         record
@@ -37,25 +25,4 @@ package instruction_decode is
             write_data: word;
         end record;
 
-    function decode_use_pc_next_address(op: opcode) return std_logic;
-
 end package instruction_decode;
-
-package body instruction_decode is
-
-    -- Returns one if the next address logic should be used or if a calculated
-    -- address should be used given an opcode.
-    --
-    function decode_use_pc_next_address(op: opcode) return std_logic is
-    begin
-
-        if op = OP_J
-        or op = OP_BR then
-            return '0';
-        else
-            return '1';
-        end if;
-
-    end function;
-
-end package body instruction_decode;
