9dbf1df54b914f114325d2f4850dd553ae957bd5
nilewapp <robert.welin@gmail.com>
2014-06-03 18:12:20 +0100

    Add test with two cores that subtract a number and pass it between themselves

---
+++ workspace/test/programs/asm/twocore.s
@@ -0,0 +1,62 @@
+
+    movr    r0  10
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+    nop 
+loop:
+    movfr   r0  0
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    subr    r0  1
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    movrf   r0  0
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    J       loop       
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+
+end:
+    J   end
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
