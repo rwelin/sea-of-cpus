7ff0ff502357a34649e7785b644b3c6ab93a3b70
nilewapp <robert.welin@gmail.com>
2014-06-19 12:34:49 +0100

    Even more report stuff

--- report/final/twocore.s
+++ report/final/twocore.s
@@ -1,62 +1,11 @@
-
     movr    r0  10
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
+    ...
 loop:
     subr    r0  1
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     movfr   r0  0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     movrf   r0  0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     J       loop       
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-
-
-end:
-    J   end
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
--- report/final/twocore2.s
+++ report/final/twocore2.s
@@ -1,62 +1,9 @@
-
-    movr    r0  10
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
-    nop 
 loop:
     movrf   r0  0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     subr    r0  1
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     movfr   r0  0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
     J       loop       
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-
-
-end:
-    J   end
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    ...
